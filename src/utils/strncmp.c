c13cbec821bb036cfb77b43bbc244fc3b23a4ae8
Ambroise Bareux <abareux@k1r3p5.42mulhouse.fr>
2024-01-11 11:50:42 +0100

    segfault

--- src/utils/strncmp.c
+++ src/utils/strncmp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                                            */
-/*   NOOT NOOT MOTHER FUCKER                      :#:  :#:         :#:  :#:   */
+/*   strncmp.c                                          :+:      :+:    :+:   */
 /*                                                :#:  :#::#     #::#:  :#:   */
 /*   By: an asshole who like to break thing       :#:  :#::#: # :#::#:  :#:   */
 /*                                                :##::##: :#:#:#: :##::##:   */
 /*   Created: the-day-it-was created by UwU        :####:  :##:##:  :####:    */
-/*   Updated: the-day-it-was updated by UwU                                   */
+/*   Updated: 2024/01/11 11:49:43 by abareux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@ int	ft_strncmp(const char *s1, const char *s2, int len)
 	int	cursor;
 
 	cursor = 0;
+	if (!s1 || !s2)
+		return (1);
 	while (*(s1 + cursor) && *(s2 + cursor) && cursor < len)
 	{
 		if (*(s1 + cursor) != *(s2 + cursor))
